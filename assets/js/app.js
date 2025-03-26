09dd4ef5e6e86ff6c4b3b509405f24c9c648683f
TUSHAR SWAMI <tusharswami17@gamil.com>
2025-03-26 09:59:39 +0530

    add faq

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,40 +1,40 @@
 const handelClick = () => {
-    document.body.classList.toggle("overflow-hidden");
-    document.getElementById("toggle-line-1").classList.toggle("translate-x-10");
-    document.getElementById("toggle-line-2").classList.toggle("rotate-45");
-    document.getElementById("toggle-line-2").classList.toggle("after:rotate-90");
-    document.getElementById("toggle-line-3").classList.toggle("-translate-x-10");
-    document.getElementById("menu").classList.toggle("max-sm:!left-0");
-    document.getElementById("menu").classList.toggle("max-sm:!top-0");
+  document.body.classList.toggle("overflow-hidden");
+  document.getElementById("toggle-line-1").classList.toggle("translate-x-10");
+  document.getElementById("toggle-line-2").classList.toggle("rotate-45");
+  document.getElementById("toggle-line-2").classList.toggle("after:rotate-90");
+  document.getElementById("toggle-line-3").classList.toggle("-translate-x-10");
+  document.getElementById("menu").classList.toggle("max-sm:!left-0");
+  document.getElementById("menu").classList.toggle("max-sm:!top-0");
 };
 
 // Swiper
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 0,
-    loop: true,
-    autoplay: {
-        delay: 3000,
-        disableOnInteraction: false,
+  slidesPerView: 3,
+  spaceBetween: 0,
+  loop: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".latest-next",
+    prevEl: ".latest-prev",
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 0,
     },
-    navigation: {
-        nextEl: ".latest-next",
-        prevEl: ".latest-prev",
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 0,
     },
-    breakpoints: {
-        320: {
-            slidesPerView: 1,
-            spaceBetween: 0,
-        },
-        768: {
-            slidesPerView: 2,
-            spaceBetween: 0,
-        },
-        1024: {
-            slidesPerView: 3,
-            spaceBetween: 0,
-        },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 0,
     },
+  },
 });
 
 const decrementButton = document.getElementById("reduce-button");
@@ -44,47 +44,48 @@ const valueDisplay = document.getElementById("output");
 let value = 100;
 
 decrementButton.addEventListener("click", () => {
-    value -= 1;
-    valueDisplay.textContent = value;
+  value -= 1;
+  valueDisplay.textContent = value;
 });
 
 incrementButton.addEventListener("click", () => {
-    value += 1;
-    valueDisplay.textContent = value;
+  value += 1;
+  valueDisplay.textContent = value;
 });
 
 // Team swiper
 var swiper = new Swiper(".team-mySwiper", {
-    slidesPerView: 2,
-    spaceBetween: 0,
-    loop: true,
-    autoplay: {
-        delay: 3000,
-        disableOnInteraction: false,
+  slidesPerView: 2,
+  spaceBetween: 0,
+  loop: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".team-next",
+    prevEl: ".team-prev",
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 0,
     },
-    navigation: {
-        nextEl: ".team-next",
-        prevEl: ".team-prev",
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 0,
     },
-    breakpoints: {
-        320: {
-            slidesPerView: 1,
-            spaceBetween: 0,
-        },
-        768: {
-            slidesPerView: 2,
-            spaceBetween: 0,
-        },
-        1024: {
-            slidesPerView: 2,
-            spaceBetween: 0,
-        },
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 0,
     },
+  },
 });
 
 // year
 const currentYear = new Date().getFullYear();
 document.getElementById("year").textContent = currentYear;
+
 // Faq Section
 function toggleAccordion(button) {
   const allAccordions = document.querySelectorAll(".accordin-data");
@@ -103,16 +104,15 @@ function toggleAccordion(button) {
 
   allIcons.forEach((otherIcon) => {
     if (otherIcon !== icon) {
-      otherIcon.style.transform = "rotate(0deg)";
+      otherIcon.classList.add("after:rotate-90");
     }
   });
 
   if (paragraph.style.maxHeight) {
     paragraph.style.maxHeight = null;
-    icon.style.transform = "rotate(0deg)";
+    icon.classList.add("after:rotate-90");
   } else {
     paragraph.style.maxHeight = paragraph.scrollHeight + "px";
-    icon.style.transform = "rotate(180deg)";
+    icon.classList.remove("after:rotate-90");
   }
 }
-
